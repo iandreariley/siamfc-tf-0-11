d9d12dcaa695153a68cf8cdb270171ac61e01652
Ian Riley <iandreariley@gmail.com>
2017-10-21 17:37:58 +0000

    Change visualizion to use cv2. It's faster.

--- src/visualization.py
+++ src/visualization.py
@@ -1,18 +1,31 @@
 import numpy as np
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
+import cv2
 
 
 def show_frame(frame, bbox, fig_n):
-    fig = plt.figure(fig_n)
-    ax = fig.add_subplot(111)
-    r = patches.Rectangle((bbox[0],bbox[1]), bbox[2], bbox[3], linewidth=2, edgecolor='r', fill=False)
-    ax.imshow(np.uint8(frame))
-    ax.add_patch(r)
-    plt.ion()
-    plt.show()
-    plt.pause(0.001)
-    plt.clf()
+    rev_frame = np.copy(frame[...,::-1]) # copy necessary to keep cv2 from throwing a fit.
+    draw_bbox(rev_frame, bbox, (0, 0, 255))
+    cv2.imshow('main', rev_frame)
+    cv2.waitKey(1)
+
+
+def draw_bbox(image, bbox, color):
+    top_left = intify((bbox[0], bbox[1]))
+    bottom_right = intify((bbox[0] + bbox[2], bbox[1] + bbox[3]))
+    top_right = (bottom_right[0], top_left[1])
+    bottom_left = (top_left[0], bottom_right[1])
+
+    # draw bounding box.
+    cv2.line(image, top_left, top_right, color, 2)
+    cv2.line(image, top_right, bottom_right, color, 2)
+    cv2.line(image, bottom_right, bottom_left, color, 2)
+    cv2.line(image, bottom_left, top_left, color, 2)
+
+
+def intify(tup):
+    return tuple(map(int, tup))
 
 
 def show_crops(crops, fig_n):
